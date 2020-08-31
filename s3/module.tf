a3386fc096e6bc74c1177f0b22d2775f2369cc10
WoodProgrammer <emirozbir@kloia.com>
2020-08-31 17:28:38 +0300

    s3 module added

---
+++ s3/module.tf
@@ -0,0 +1,48 @@
+resource "aws_s3_bucket" "bucket" {
+
+  bucket = var.bucket_name
+  acl    = var.acl
+
+  tags = var.tags
+
+  versioning {
+    enabled = var.is_vers_enabled
+  }
+
+
+  lifecycle_rule {
+    
+    id      = "lifecycle=${var.bucket_name}"
+    enabled = var.is_lifecycle_enabled
+
+    prefix = ""
+
+    transition {
+      days          = var.short_storage_day
+      storage_class = var.storage_class
+    }
+
+    transition {
+      days          = var.long_storage_day
+      storage_class = var.long_storage_class
+    }
+
+    expiration {
+      days = var.expiration
+    }
+  }
+
+
+  force_destroy = var.force_destroy
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.bucket.bucket
+}
+
+
+output "s3_origin_id" {
+  value = aws_s3_bucket.bucket.bucket_domain_name
+}
+
+
---
+++ s3/variables.tf
@@ -0,0 +1,54 @@
+variable "bucket_name" {
+    type = string
+    description = "Name of bucket"
+}
+
+variable "acl" {
+    default = "private"
+}
+
+variable "tags" {
+    type = map(string)
+    description = "Tags for bucket"
+}
+
+variable "is_vers_enabled" {
+    type = bool
+    description = "Enable versioning"
+    default = false
+}
+
+variable "is_lifecycle_enabled" {
+    type = bool
+    description = "Enable lifecycle"
+    default = false
+}
+
+variable "short_storage_day" {
+    type = string
+    default =  40
+}
+
+variable "long_storage_day" {
+    type = string
+    default =  40
+}
+
+variable "storage_class" {
+  default = "STANDARD_IA"
+}
+
+variable "long_storage_class" {
+  default = "GLACIER"
+}
+
+variable "force_destroy" {
+    type = bool
+    description = "Force to destroy bucket"
+    default = false
+}
+
+variable "expiration" {
+    default     = 90
+
+}
